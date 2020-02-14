c38240b6aa7e14d53d4a55de602d10b09d540156
Mr.Yan <48596931+CrazyMrYan@users.noreply.github.com>
2020-02-14 13:47:08 +0800

    Delete 404.js

--- src/404.js
+++
@@ -1,3 +0,0 @@
-if (localStorage.getItem('token') == '' || localStorage.getItem('token') == undefined || localStorage.getItem('token') == null || !!localStorage.getItem('token')) {
-    location.href = '../../404.html'
-}
